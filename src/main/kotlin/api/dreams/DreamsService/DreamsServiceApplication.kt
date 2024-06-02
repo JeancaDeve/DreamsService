09bd98be0cdc46a81b00e6921ecff7d31104b79c
JeancaDeve <khevinelpro@gmail.com>
2024-06-02 10:14:02 -0500

    Api para la reserva de habitaciones

---
+++ src/main/kotlin/api/dreams/DreamsService/DreamsServiceApplication.kt
@@ -0,0 +1,11 @@
+package api.dreams.DreamsService
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DreamsServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<DreamsServiceApplication>(*args)
+}
---
+++ src/test/kotlin/api/dreams/DreamsService/DreamsServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package api.dreams.DreamsService
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DreamsServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
